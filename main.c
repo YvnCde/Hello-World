0b022274bc940016e5d628ab2bca48c212936db4
Yvan Gauthier <y.gauthier@keemail.me>
2024-08-11 17:55:18 +0200

    First and last commit for this small program

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+
+int main(void) {
+	// This is a trivial program to check if everything is working fine. 
+	printf("######################\n"); 
+	printf(" Hello, World !\n"); 
+	printf("######################\n"); 
+	return 0; 	
+}
